065152888ae024660c431ae8b2c2aad5b54f5a4f
Aga <kocagnes@gmail.com>
2019-03-01 16:27:33 +0000

    Enable dataset generation from commandline.

--- src/generate_dataset.py
+++ src/generate_dataset.py
@@ -1,16 +1,19 @@
+import argparse
+import getopt
+import sys
+
 from handle_datasets import save_dataset
 
-def generate_datasets(argv):
+
+def generate_datasets(topics):
+    print(topics)
+    for topic in topics:
+        print(topic)
+        save_dataset(topic)
 
 
 if __name__ == "__main__":
-    generate_datasets()
-    save_dataset("trump")
-    save_dataset("obamacare")
-    save_dataset("immigration")
-    save_dataset("nra")
-    save_dataset("miley cirus")
-    save_dataset("israel")
-    save_dataset("video game violence")
-    save_dataset("voting age")
-    save_dataset("antivax")
+    parser = argparse.ArgumentParser(description="Generate datasets on passed topics.")
+    parser.add_argument("topics", nargs="+", action="append", help="Topics to be queried on Twitter.")
+    args = parser.parse_args()
+    generate_datasets(args.topics[0])
