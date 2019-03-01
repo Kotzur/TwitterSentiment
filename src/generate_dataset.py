00150bd880c3d13ccefd13173e0548f3a964d238
Aga <kocagnes@gmail.com>
2019-03-01 16:26:41 +0000

    File changes.

---
+++ src/generate_dataset.py
@@ -0,0 +1,16 @@
+from handle_datasets import save_dataset
+
+def generate_datasets(argv):
+
+
+if __name__ == "__main__":
+    generate_datasets()
+    save_dataset("trump")
+    save_dataset("obamacare")
+    save_dataset("immigration")
+    save_dataset("nra")
+    save_dataset("miley cirus")
+    save_dataset("israel")
+    save_dataset("video game violence")
+    save_dataset("voting age")
+    save_dataset("antivax")
