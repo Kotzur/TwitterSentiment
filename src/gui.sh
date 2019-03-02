2dce49bd8cdbefb038204ab2fc47b81fc7dfd3de
Aga <kocagnes@gmail.com>
2019-03-02 07:51:33 +0000

    Adjust bash script to recent changes.

--- src/gui.sh
+++ src/gui.sh
@@ -2,16 +2,16 @@
 # Shell script to run the Dart GUI for user study.
 
 # Run the flask server.
-pipenv run python server.py &
+pipenv run python server.py -s $1 &
 
 # Host the interface
 URL="$PWD/interface/build/index.html"
-if which xdg-open > /xdev/null
-then
-  echo "xdg"
+#if which xdg-open > /xdev/null
+#then
+#  echo "xdg"
   xdg-open "$URL"
-elif which gnome-open > /dev/null
-then
-    echo "gnome"
-  gnome-open "$URL"
-fi
+#elif which gnome-open > /dev/null
+#then
+#    echo "gnome"
+#  gnome-open "$URL"
+#fi
